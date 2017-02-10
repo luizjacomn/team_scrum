45903ea4866a339a6f99c890e37794927645ce6d
luizjacomn <luizjacomn@gmail.com>
2017-02-09 23:34:35 -0300

    CRUD Organização (Back end)

--- src/main/java/com/github/teamscrum/ws/model/Organizacao.java
+++ src/main/java/com/github/teamscrum/ws/model/Organizacao.java
@@ -6,24 +6,38 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
 @Entity
 public class Organizacao {
 
 	private Long id;
 	private String nome;
-	// private String logradouro;
-	// private Integer numero;
-	// private String complemtento;
-	// private String bairro;
-	// private String cidade;
-	// private String uf;
-	// private String cep;
+	private String logradouro;
+	private Integer numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	
+	public Organizacao() {
+	
+	}
+	
+	public Organizacao(String nome, String logradouro, Integer numero, String complemento, String bairro, String cidade,
+			String estado, String cep) {
+		this.nome = nome;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+	}
 
 	// GETTERS AND SETTERS
-	 @Id
-	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
@@ -32,14 +46,80 @@ public class Organizacao {
 		this.id = id;
 	}
 
-	 @Column(name = "nome")
-	 @NotEmpty(message = "O campo nome e obrigatorio!")
+	@Column(name = "nome")
+//	@NotEmpty(message = "O campo nome e obrigatorio!")
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Column(name = "logradouro")
+//	@NotEmpty(message = "O campo logradouro e obrigatorio!")
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	@Column(name = "numero")
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	@Column(name = "bairro")
+//	@NotEmpty(message = "O campo bairro e obrigatorio!")
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	@Column(name = "cidade")
+//	@NotEmpty(message = "O campo cidade e obrigatorio!")
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	@Column(name = "estado", length = 2)
+//	@NotEmpty(message = "O campo estado e obrigatorio!")
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	@Column(name = "cep")
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 	@Override
